1358c7edd2124220ba380da033881425eccebbcc
givdieri <75476306+givdieri@users.noreply.github.com>
2024-12-18 00:19:32 +0100

    Update dependencies.sh

--- scripts_altered/dependencies.sh
+++ scripts_altered/dependencies.sh
@@ -21,7 +21,7 @@ export PORECHOP_UMI=porechop
 export FILTLONG=filtlong
 export BWA=bwa
 export USEARCH="path/to/usearch"
-export MEDAKA_ENV_START="conda activate medaka"
+export MEDAKA_ENV_START="source /opt/conda/etc/profile.d/conda.sh && conda activate medaka"
 export MEDAKA_ENV_STOP="conda deactivate"
 export VSEARCH="path/to/vsearch"
 
